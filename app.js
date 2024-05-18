b69e534ea19c8f1bf374f0df39f35bbc81caf2a9
Aslan <isip_a.g.asalbekov@mpt.ru>
2024-05-18 19:38:07 +0300

    Metrics

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import usernamesRouter from './Queryies/requests.js';
+import { collectDefaultMetrics, register } from "prom-client";
 
+collectDefaultMetrics();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
 app.use('/', usernamesRouter);
+
+app.get('/metrics', async (_req, res) => {
+   try {
+       res.set('Content-Type', register.contentType);
+       res.end(await register.metrics());
+   } catch (err) {
+       res.status(500);
+   }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
